2c3bd6266b5f91049e7782afe6c0ff678cfebd5d
Joseph Bremser <jbremser@c1r2p2.hive.fi>
2024-12-17 12:04:47 +0200

    ad

--- libft/ft_strcmp.c
+++ libft/ft_strcmp.c
@@ -6,13 +6,18 @@
 /*   By: jbremser <jbremser@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 12:54:55 by jbremser          #+#    #+#             */
+<<<<<<< HEAD
 /*   Updated: 2024/08/08 13:11:26 by jbremser         ###   ########.fr       */
+=======
+/*   Updated: 2024/09/24 16:00:42 by jbremser         ###   ########.fr       */
+>>>>>>> 5dbf18f (added libft)
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "libft.h"
 
+<<<<<<< HEAD
 int ft_strcmp(const char *s1, const char *s2)
 {
     unsigned char	*s1a;
@@ -24,6 +29,19 @@ int ft_strcmp(const char *s1, const char *s2)
 	s2a = (unsigned char *)s2;
     while ((s1a[i]) || (s2a[i]))
     {
+=======
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	unsigned char	*s1a;
+	unsigned char	*s2a;
+	size_t			i;
+
+	i = 0;
+	s1a = (unsigned char *)s1;
+	s2a = (unsigned char *)s2;
+	while ((s1a[i]) || (s2a[i]))
+	{
+>>>>>>> 5dbf18f (added libft)
 		if (s1a[i] != s2a[i])
 			return (s1a[i] - s2a[i]);
 		else
